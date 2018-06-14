6f6bebe5b0d0032a76f25a42469507029f17a0f4
Sebastian <sebastian@squidex.io>
2018-06-14 07:08:44 +0200

    Started with karma config.

---
+++ config/webpack.test.coverage.js
@@ -0,0 +1,35 @@
+﻿const webpack = require('webpack'),
+ webpackMerge = require('webpack-merge'),
+         path = require('path'),
+      helpers = require('./helpers'),
+   testConfig = require('./webpack.test.js');
+
+helpers.removeLoaders(testConfig, ['ts']);
+
+module.exports = webpackMerge(testConfig, {
+    module: {        
+        /**
+         * An array of Rules which are matched to requests when modules are created.
+         *
+         * See: https://webpack.js.org/configuration/module/#module-rules
+         */
+        rules: [{
+            test: /\.ts$/,
+            use: [{
+                loader: 'awesome-typescript-loader'
+            }],
+            include: [/\.(e2e|spec)\.ts$/],
+            
+        }, {
+            test: /\.ts$/,
+            use: [{
+                loader: 'istanbul-instrumenter-loader'
+            }, {
+                loader: 'awesome-typescript-loader'
+            }, {
+                loader: 'tslint-loader' 
+            }],
+            exclude: [/\.(e2e|spec)\.ts$/]
+        }]
+    }
+});
