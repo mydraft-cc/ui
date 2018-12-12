67f77c85a1b0f95aa00b9268679311ea8a54bb39
Sebastian Stehle <sebastian@squidex.io>
2018-12-12 09:24:15 +0100

    Phone updated.

--- src/wireframes/shapes/neutral/phone.ts
+++ src/wireframes/shapes/neutral/phone.ts
@@ -13,33 +13,33 @@ export class Phone extends AbstractControl {
     }
 
     public createDefaultShape(shapeId: string): DiagramShape {
-        return DiagramShape.createShape(shapeId, this.identifier(), 480, 860, undefined, DEFAULT_APPEARANCE);
+        return DiagramShape.createShape(shapeId, this.identifier(), 360, 640, undefined, DEFAULT_APPEARANCE);
     }
 
     protected renderInternal(ctx: AbstractContext) {
-        const hullItem = ctx.renderer.createRectangle(0, 20, ctx.bounds);
+        const hullRect = new Rect2(-20, -80, ctx.bounds.width + 40, ctx.bounds.height + 140);
+        const hullItem = ctx.renderer.createRectangle(0, 20, hullRect);
 
         ctx.renderer.setBackgroundColor(hullItem, 0);
 
         ctx.add(hullItem);
 
         if (ctx.bounds.width >= 50 && ctx.bounds.height > 200) {
-            this.createDisplay(ctx);
+            this.createScreen(ctx);
             this.createSpeaker(ctx);
         }
     }
 
-    private createDisplay(ctx: AbstractContext) {
-        const displayRect = new Rect2(20, 100, ctx.bounds.width - 40, ctx.bounds.height - 180);
-        const displayItem = ctx.renderer.createRectangle(0, 0, displayRect);
+    private createScreen(ctx: AbstractContext) {
+        const screenItem = ctx.renderer.createRectangle(0, 0, ctx.bounds);
 
-        ctx.renderer.setBackgroundColor(displayItem, ctx.shape);
+        ctx.renderer.setBackgroundColor(screenItem, ctx.shape);
 
-        ctx.add(displayItem);
+        ctx.add(screenItem);
     }
 
     private createSpeaker(ctx: AbstractContext) {
-        const speakerRect = new Rect2((ctx.bounds.width - 50) * 0.5, 48, 50, 4);
+        const speakerRect = new Rect2((ctx.bounds.width - 50) * 0.5, -35, 50, 4);
         const speakerItem = ctx.renderer.createRectangle(0, 2, speakerRect);
 
         ctx.renderer.setBackgroundColor(speakerItem, 0x333333);
