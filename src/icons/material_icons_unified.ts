902fa71ea018b0147ec149e7d505220eb84313f2
Sebastian <sebastian@squidex.io>
2018-12-29 14:33:10 +0100

    Build fix.

--- src/icons/material_icons_unified.ts
+++ src/icons/material_icons_unified.ts
@@ -1,6 +1,6 @@
 import { ICONS } from './material_icons';
 
-export const ICONS_MATERIAL_DESIGN = ICONS.split('\n').map(icon => {
+export const ICONS_MATERIAL_DESIGN = ICONS.map(icon => {
     const parts = icon.trim().split(' ');
 
     const text = String.fromCharCode(parseInt(parts[1], 16));
