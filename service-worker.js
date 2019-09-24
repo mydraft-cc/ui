dab8f75fc3856f8b68518920ec9e001b6ca89bf5
SebastianStehle <sebastian@squidex.io>
2019-09-24 14:41:04 +0000

    commit 7a21ee691e2912a6861bb780c868b872c602492d Author: Sebastian <sebastian@squidex.io> Date:   Tue Sep 24 16:37:49 2019 +0200
        Service worker improved.

--- service-worker.js
+++ service-worker.js
@@ -229,7 +229,7 @@ self.addEventListener('fetch', function(event) {
 
     // If shouldRespond is still false, check to see if this is a navigation
     // request, and if so, whether the URL matches navigateFallbackWhitelist.
-    var navigateFallback = '';
+    var navigateFallback = 'https://mydraft.cc/index.html';
     if (!shouldRespond &&
         navigateFallback &&
         (event.request.mode === 'navigate') &&
