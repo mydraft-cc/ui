a3653d4d6b2f6a90975c698e9395ffd77e791140
Sebastian <sebastian@squidex.io>
2022-09-15 19:25:45 +0200

    Add missing items

---
+++ src/wireframes/components/properties/DiagramProperties.tsx
@@ -0,0 +1,65 @@
+/*
+ * mydraft.cc
+ *
+ * @license
+ * Copyright (c) Sebastian Stehle. All rights reserved.
+*/
+
+import { Col, InputNumber, Row } from 'antd';
+import * as React from 'react';
+import { useDispatch } from 'react-redux';
+import { Color, ColorPicker } from '@app/core';
+import { texts } from '@app/texts';
+import { changeColor, changeSize, getEditor, useStore } from '@app/wireframes/model';
+
+export const DiagramProperties = React.memo(() => {
+    const dispatch = useDispatch();
+    const editor = useStore(getEditor);
+    const editorSize = editor.size;
+    const editorColor = editor.color;
+    const [sizeWidth, setWidth] = React.useState(0);
+    const [sizeHeight, setHeight] = React.useState(0);
+    const [color, setColor] = React.useState(Color.WHITE);
+
+    React.useEffect(() => {
+        setWidth(editorSize.x);
+        setHeight(editorSize.y);
+    }, [editorSize]);
+
+    React.useEffect(() => {
+        setColor(editorColor);
+    }, [editorColor]);
+
+    const doChangeSize = React.useCallback(() => {
+        dispatch(changeSize({ width: sizeWidth, height: sizeHeight }));
+    }, [dispatch, sizeWidth, sizeHeight]);
+
+    const doChangeColor = React.useCallback((color: Color) => {
+        dispatch(changeColor({ color: color.toString() }));
+    }, [dispatch]);
+
+    return (
+        <>
+            <Row className='property'>
+                <Col span={12} className='property-label'>{texts.common.width}</Col>
+                <Col span={12} className='property-value'>
+                    <InputNumber value={sizeWidth} min={100} max={3000} onChange={setWidth} onBlur={doChangeSize} />
+                </Col>
+            </Row>
+
+            <Row className='property'>
+                <Col span={12} className='property-label'>{texts.common.height}</Col>
+                <Col span={12} className='property-value'>
+                    <InputNumber value={sizeHeight} min={100} max={3000} onChange={setHeight} onBlur={doChangeSize} />
+                </Col>
+            </Row>
+
+            <Row className='property'>
+                <Col span={12} className='property-label'>{texts.common.backgroundColor}</Col>
+                <Col span={12} className='property-value'>
+                    <ColorPicker value={color} onChange={doChangeColor} />
+                </Col>
+            </Row>
+        </>
+    );
+});
---
+++ src/wireframes/components/properties/Properties.tsx
@@ -0,0 +1,47 @@
+/*
+ * mydraft.cc
+ *
+ * @license
+ * Copyright (c) Sebastian Stehle. All rights reserved.
+*/
+
+import { Collapse } from 'antd';
+import classNames from 'classnames';
+import * as React from 'react';
+import { texts } from '@app/texts';
+import { getDiagram, getSelectedItems, useStore } from '@app/wireframes/model';
+import { CustomProperties } from './CustomProperties';
+import { DiagramProperties } from './DiagramProperties';
+import { LayoutProperties } from './LayoutProperties';
+import { MoreProperties } from './MoreProperties';
+import { VisualProperties } from './VisualProperties';
+
+export const Properties = () => {
+    const hasSelection = useStore(getSelectedItems).length > 0;
+    const hasDiagram = !!useStore(getDiagram);
+
+    return (
+        <>
+            <Collapse className={(classNames({ hidden: !hasSelection }))} bordered={false} defaultActiveKey={['layout', 'visual', 'more', 'custom']}>
+                <Collapse.Panel key='layout' header={texts.common.layout}>
+                    <LayoutProperties />
+                </Collapse.Panel>
+                <Collapse.Panel key='visual' header={texts.common.visual}>
+                    <VisualProperties />
+                </Collapse.Panel>
+                <Collapse.Panel key='more' header={texts.common.more}>
+                    <MoreProperties />
+                </Collapse.Panel>
+                <Collapse.Panel key='custom' header={texts.common.custom}>
+                    <CustomProperties />
+                </Collapse.Panel>
+            </Collapse>
+
+            <Collapse className={(classNames({ hidden: hasSelection || !hasDiagram }))} bordered={false} defaultActiveKey={['diagram']}>
+                <Collapse.Panel key='diagram' header={texts.common.diagram}>
+                    <DiagramProperties />
+                </Collapse.Panel>
+            </Collapse>
+        </>
+    );
+};
