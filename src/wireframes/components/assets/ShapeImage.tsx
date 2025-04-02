d23ccf7391dacb594ff60d439ad8e711f79126a6
Brent Nowak <bnowak008@gmail.com>
2025-04-02 11:33:05 -0500

    tweak dragging from toolbar.

--- src/wireframes/components/assets/ShapeImage.tsx
+++ src/wireframes/components/assets/ShapeImage.tsx
@@ -6,6 +6,8 @@
 */
 
 import * as React from 'react';
+import { useDrag } from 'react-dnd';
+import { getEmptyImage } from 'react-dnd-html5-backend';
 import { ShapeInfo } from '@app/wireframes/model';
 import { ShapeRenderer } from '@app/wireframes/shapes/ShapeRenderer';
 import { AppTheme } from '@app/wireframes/interface';
@@ -23,9 +25,26 @@ const DESIRED_HEIGHT = 72;
 export const ShapeImage = React.memo((props: ShapeImageProps) => {
     const { appTheme, shape } = props;
 
+    const [, drag, connectDragPreview] = useDrag({
+        item: shape,
+        previewOptions: {
+            anchorX: 0,
+            anchorY: 0,
+        },
+        type: 'DND_ASSET',
+    });
+
+    React.useEffect(() => {    
+        connectDragPreview(getEmptyImage(), {
+            anchorX: 0,
+            anchorY: 0,
+            captureDraggingState: true,
+        });
+    }, [connectDragPreview]);
+
     return (
         <div className='asset-shape-image'>
-            <ShapeRenderer
+            <ShapeRenderer ref={drag}
                 appTheme={appTheme}
                 desiredHeight={DESIRED_HEIGHT} 
                 desiredWidth={DESIRED_WIDTH} 
