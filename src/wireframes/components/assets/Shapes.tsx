a7d7f7020c935a7b946f9bd0e26986cacb390f50
Brent Nowak <bnowak008@gmail.com>
2025-04-02 14:34:17 -0500

    Fix react hook issue.

--- src/wireframes/components/assets/Shapes.tsx
+++ src/wireframes/components/assets/Shapes.tsx
@@ -14,6 +14,7 @@ import { AppState, useAppDispatch, useAppSelector } from '@app/store';
 import { texts } from '@app/texts';
 import { addShape, filterShapes, getDiagramId, getFilteredShapes, getShapesFilter, ShapeInfo, useStore } from '@app/wireframes/model';
 import { selectEffectiveAppTheme } from '@app/wireframes/model/selectors/themeSelectors';
+import { AppTheme } from '@app/wireframes/interface';
 import { ShapeImage } from './ShapeImage';
 import { useDrag } from 'react-dnd';
 import { getEmptyImage } from 'react-dnd-html5-backend';
@@ -23,6 +24,40 @@ const keyBuilder = (shape: ShapeInfo) => {
     return shape.name;
 };
 
+interface ShapeItemProps {
+    shape: ShapeInfo;
+    appTheme: AppTheme;
+    onAdd: (shape: ShapeInfo) => void;
+}
+
+const ShapeItem: React.FC<ShapeItemProps> = ({ shape, appTheme, onAdd }) => {
+    const [, drag, connectDragPreview] = useDrag({
+        item: shape,
+        previewOptions: {
+            anchorX: 0,
+            anchorY: 0,
+        },
+        type: 'DND_ASSET',
+    });
+
+    React.useEffect(() => {    
+        connectDragPreview(getEmptyImage(), {
+            anchorX: 0,
+            anchorY: 0,
+            captureDraggingState: true,
+        });
+    }, [connectDragPreview]);
+
+    return (
+        <div className='asset-shape' ref={drag} onDoubleClick={() => onAdd(shape)}>
+            <div className='asset-shape-image-row'>
+                <ShapeImage shape={shape} appTheme={appTheme} />
+            </div>
+            <div className='asset-shape-title'>{shape.displayName}</div>
+        </div>
+    );
+};
+
 export const Shapes = () => {
     const dispatch = useAppDispatch();
     const shapesFiltered = useStore(getFilteredShapes);
@@ -30,41 +65,23 @@ export const Shapes = () => {
     const store = useReduxStore<AppState>();
     const appTheme = useAppSelector(selectEffectiveAppTheme);
 
+    const doAdd = React.useCallback((shape: ShapeInfo) => {
+        const selectedDiagramId = getDiagramId(store.getState());
+
+        if (selectedDiagramId) {
+            dispatch(addShape(selectedDiagramId, shape.name, { position: { x: 100, y: 100 } }));
+        }
+    }, [dispatch, store]);
+
     const cellRenderer = React.useCallback((shape: ShapeInfo) => {
-        const doAdd = () => {
-            const selectedDiagramId = getDiagramId(store.getState());
-
-            if (selectedDiagramId) {
-                dispatch(addShape(selectedDiagramId, shape.name, { position: { x: 100, y: 100 } }));
-            }
-        };
-
-        const [, drag, connectDragPreview] = useDrag({
-            item: shape,
-            previewOptions: {
-                anchorX: 0,
-                anchorY: 0,
-            },
-            type: 'DND_ASSET',
-        });
-
-        React.useEffect(() => {    
-            connectDragPreview(getEmptyImage(), {
-                anchorX: 0,
-                anchorY: 0,
-                captureDraggingState: true,
-            });
-        }, [connectDragPreview]);
-
         return (
-            <div className='asset-shape' ref={drag} onDoubleClick={doAdd}>
-                <div className='asset-shape-image-row'>
-                    <ShapeImage shape={shape} appTheme={appTheme} />
-                </div>
-                <div className='asset-shape-title'>{shape.displayName}</div>
-            </div>
+            <ShapeItem 
+                shape={shape} 
+                appTheme={appTheme} 
+                onAdd={doAdd}
+            />
         );
-    }, [dispatch, store, appTheme]);
+    }, [doAdd, appTheme]);
 
     const doFilterShapes = useEventCallback((event: React.ChangeEvent<HTMLInputElement>) => {
         dispatch(filterShapes(event.target.value));
